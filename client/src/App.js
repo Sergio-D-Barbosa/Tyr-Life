4841afcb8ea3853b784e1e8df7e0c2aa996ce725
Sérgio <88911524+Sergio-D-Barbosa@users.noreply.github.com>
2022-01-26 09:30:04 -0300

    version init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './componentes/header.js'
+
+function App() {
+  return (
+    <>
+      <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/componentes/header.js
@@ -0,0 +1,12 @@
+export default function header(){
+    return(
+    <header>
+        <nav>
+            <ul>
+                <li>Tarefas</li>
+                <li>Habilidades</li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
